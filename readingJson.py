ae0464b8855b51ae1d7956d27c4cb0202b3ae282
SUPAWAT SIRINTRANON <supawat.sir@dome.tu.ac.th>
2018-05-25 20:06:00 +0700

    secondcommit

--- readingJson.py
+++ readingJson.py
@@ -3,5 +3,6 @@ from pprint import pprint
 
 with open('trafficCapture.json') as f:
     data = json.load(f)
-
-pprint(data)
+    for i in data.values():
+        pprint(data["log"]["entries"][0]["request"]["url"])
+# pprint(data["log"]["entries"][0]["request"]["url"])
